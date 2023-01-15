c065d99d257ce15b0a90e485fb3ac577df6cf1d2
vulcanic <vulcanic101@gmail.com>
2023-01-15 23:36:02 +0000

    wooden golem prototype

--- src/main/java/land/alfred/souls/blocks/SoulfulPumpkinBlock.java
+++ src/main/java/land/alfred/souls/blocks/SoulfulPumpkinBlock.java
@@ -1,14 +1,38 @@
 package land.alfred.souls.blocks;
 
+import land.alfred.souls.registry.BlockRegistry;
+import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.CarvedPumpkinBlock;
+import net.minecraft.block.Material;
+import net.minecraft.block.pattern.BlockPattern;
+import net.minecraft.block.pattern.BlockPatternBuilder;
+import net.minecraft.block.pattern.CachedBlockPosition;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.entity.passive.SnowGolemEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.predicate.block.BlockStatePredicate;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.function.MaterialPredicate;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
+import java.util.Iterator;
+import java.util.function.Predicate;
+
 public class SoulfulPumpkinBlock extends CarvedPumpkinBlock {
+    private BlockPattern snowGolemPattern;
+
     public SoulfulPumpkinBlock(Settings settings) {
         super(settings);
     }
@@ -30,5 +54,49 @@ public class SoulfulPumpkinBlock extends CarvedPumpkinBlock {
 
             }
         }
+    }
+    public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+        if (!oldState.isOf(state.getBlock())) {
+            this.trySpawnEntity(world, pos);
+        }
+    }
+
+    private void trySpawnEntity(World world, BlockPos pos) {
+        BlockPattern.Result result = this.getWoodGolemPattern().searchAround(world, pos);
+        if (result != null) {
+            SnowGolemEntity snowGolemEntity = (SnowGolemEntity) EntityType.SNOW_GOLEM.create(world);
+            if (snowGolemEntity != null) {
+                spawnEntity(world, result, snowGolemEntity, result.translate(0, 2, 0).getBlockPos());
+            }
+        }
+    }
+
+    private static void spawnEntity(World world, BlockPattern.Result patternResult, Entity entity, BlockPos pos) {
+        breakPatternBlocks(world, patternResult);
+        // wood golem will be 1 block tall! remove the + 1.0 for pos.getY()!
+        if (patternResult.getForwards() == Direction.NORTH || patternResult.getForwards() == Direction.SOUTH) {
+            entity.refreshPositionAndAngles((double) pos.getX() - 0.5, (double) pos.getY() + 1.0, (double) pos.getZ() + 0.5, 0.0F, 0.0F);
+        } else {
+            entity.refreshPositionAndAngles((double) pos.getX() + 0.5, (double) pos.getY() + 1.0, (double) pos.getZ() - 0.5, 0.0F, 0.0F);
+        }
+        entity.setCustomName(Text.of("Wooden Golem"));
+        entity.setCustomNameVisible(true);
+        entity.setNoGravity(true);
+        world.spawnEntity(entity);
+        Iterator var4 = world.getNonSpectatingEntities(ServerPlayerEntity.class, entity.getBoundingBox().expand(5.0)).iterator();
+
+        while(var4.hasNext()) {
+            ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity)var4.next();
+            Criteria.SUMMONED_ENTITY.trigger(serverPlayerEntity, entity);
+        }
+
+        updatePatternBlocks(world, patternResult);
+    }
+
+    private BlockPattern getWoodGolemPattern() {
+        if (this.snowGolemPattern == null) {
+            this.snowGolemPattern = BlockPatternBuilder.start().aisle(new String[]{"~^~", "-#-"}).where('^', CachedBlockPosition.matchesBlockState(BlockStatePredicate.forBlock(BlockRegistry.SOULFUL_PUMPKIN))).where('#', CachedBlockPosition.matchesBlockState(BlockStatePredicate.forBlock(Blocks.STRIPPED_OAK_LOG))).where ('-', CachedBlockPosition.matchesBlockState(BlockStatePredicate.forBlock(Blocks.OAK_BUTTON))).where('~', CachedBlockPosition.matchesBlockState(MaterialPredicate.create(Material.AIR))).build();
+        }
+        return this.snowGolemPattern;
     }
 }
--- src/main/java/land/alfred/souls/mixin/ZombieEntity.java
+++ src/main/java/land/alfred/souls/mixin/ZombieEntity.java
@@ -1,13 +1,10 @@
 package land.alfred.souls.mixin;
 
 import land.alfred.souls.registry.ItemRegistry;
-import net.minecraft.entity.mob.DrownedEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
 import net.minecraft.item.Items;
-import net.minecraft.registry.tag.ItemTags;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,12 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 @Mixin(net.minecraft.entity.mob.ZombieEntity.class)
 public abstract class ZombieEntity {
     protected ActionResult interactMob(PlayerEntity player, Hand hand) {
-        ItemStack itemStack = player.getStackInHand(hand);
-        if (itemStack == new ItemStack(Items.GLASS_BOTTLE)) {
-
-            return ActionResult.success(this.world.isClient);
-        } else {
-            return super.interactMob(player, hand);
-        }
+        System.out.println(player + " interacted!");
+        return ActionResult.FAIL;
     }
 }
